b4728a278f522c5b65eacb77db004c3fad2cd722
Bum <sbl133@naver.com>
2021-09-18 19:30:21 +0900

    feat: upload introduction project

---
+++ introduction/hello-spring/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,15 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        System.setProperty("spring.devtools.livereload.enabled", "true");
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+
+}
---
+++ introduction/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,45 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired // 스프링빈에 등록되어있는 멤버 서비스 객체를 가져옴 -> Dependency Injection
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // 데이터를 form같은데에 넣어서 전달하는걸 받을 때 PostMapping
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
