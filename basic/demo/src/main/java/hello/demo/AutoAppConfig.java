ca8fe6828eaa0dd0aa24cf5775291787c6c5b29f
Bum <sbl133@naver.com>
2021-09-24 23:24:45 +0900

    feat: Bean Scope

--- basic/demo/src/main/java/hello/demo/AutoAppConfig.java
+++ basic/demo/src/main/java/hello/demo/AutoAppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan( // @Component 에너테이션이 붙은 클래스를 찾아서 자동으로 스프링 빈으로 등록
 //        basePackages = "hello.demo.member", // 컴포넌트스캔 시작 위치를 지정해 준다.
 //        basePackageClasses = AutoAppConfig.class, // 지정한 클래스의 패키지를 시작 위치로
-//        시작위치를 지정하지 않으면 ComponentScan이 붙은 클래스의 패키지를 시작 위치로
+//        시작위치를 지정하지 않으면 ComponentScan 붙은 클래스의 패키지를 시작 위치로
         excludeFilters =
         @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 
