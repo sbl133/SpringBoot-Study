ebddfef4ef9de84007d8498d2f545b9f13f5ac30
Bum <sbl133@naver.com>
2022-05-09 15:42:51 +0900

    [feat] spring security를 이용한 session login 구현

--- security1/security1/src/main/java/com/cos/security1/cofing/auth/PrincipalDetails.java
+++ security1/security1/src/main/java/com/cos/security1/cofing/auth/PrincipalDetails.java
@@ -1,12 +1,5 @@
 package com.cos.security1.cofing.auth;
 
-// 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
-// 로그인을 진행이 완료가 되면 시큐리티 session을 만들어준다. (Security ContextHolder)
-// 오브젝트 => Authentication 타입 객체
-// Authentication 안에 User정보가 있어야 됨.
-// User 오브젝트타입 => UserDetails 타입 객체
-
-// Security Session => Authentication => UserDetails
 
 import com.cos.security1.model.User;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +9,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 
+// 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
+// 로그인을 진행이 완료가 되면 시큐리티 session을 만들어준다. (Security ContextHolder)
+// 오브젝트 => Authentication 타입 객체
+// Authentication 안에 User정보가 있어야 됨.
+// User 오브젝트타입 => UserDetails 타입 객체
+
+// Security Session => Authentication => UserDetails
 @RequiredArgsConstructor
 public class PrincipalDetails implements UserDetails {
 
