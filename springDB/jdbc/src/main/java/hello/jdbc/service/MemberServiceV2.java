f6bed584c723e7f934fff412bd71d36477c547a1
Bum <sbl133@naver.com>
2022-09-13 17:41:46 +0900

    [feat] understanding of transaction

---
+++ springDB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,67 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        Connection con = dataSource.getConnection();
+
+        try {
+            con.setAutoCommit(false);
+
+            bizLogic(fromId, toId, money, con);
+
+            con.commit();
+        } catch (Exception e) {
+            con.rollback();
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+
+    }
+
+    private void bizLogic(String fromId, String toId, int money, Connection con) throws SQLException {
+        // 비즈니스 로직
+        Member fromMember = memberRepository.findById(con, fromId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(con, toId, fromMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true);
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
