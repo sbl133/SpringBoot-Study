e76378c6740a4efa1b626f7742c3158c3ea50447
Bum <sbl133@naver.com>
2022-09-23 17:49:58 +0900

    [feat] JDK Dynamic Proxy

--- advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,8 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v1_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
@@ -14,7 +16,9 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
